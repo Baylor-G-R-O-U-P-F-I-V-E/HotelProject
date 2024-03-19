cac12df251196089c87b8ac61ea830cdf950634c
Brendon Newton <brendonnewton0@gmail.com>
2024-03-19 15:11:57 -0500

    Bug fixes for icons

--- src/ui/reserveRoom/GuestSidePanel.java
+++ src/ui/reserveRoom/GuestSidePanel.java
@@ -1,5 +1,7 @@
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.io.File;
 
 public class GuestSidePanel extends JPanel{
     public GuestSidePanel() {
@@ -10,29 +12,43 @@ public class GuestSidePanel extends JPanel{
 
     public void addButtons() {
         //Creates a default size
-        Dimension buttonSize = new Dimension(272, 251);
+        Dimension buttonSize = new Dimension(250, 250);
 
-        //Init home buttons
-        ImageIcon homeIcon = new ImageIcon("../../../resources/button-icons/home-icon.png");
-        JButton homeButton = new JButton(homeIcon);
-        homeButton.setPreferredSize(buttonSize);
-        homeButton.setMaximumSize(buttonSize);
+        //Init home button
+        try {
+            Image homeImage = ImageIO.read(new File("resources/button-icons/home-icon.png"));
+            ImageIcon homeIcon = new ImageIcon(homeImage);
+            JButton homeButton = new JButton(homeIcon);
+            homeButton.setPreferredSize(buttonSize);
+            homeButton.setMaximumSize(buttonSize);
+            add(homeButton);
+        } catch (Exception e) {
+            System.out.println("Error: home-icon.png not found");
+        }
 
         //Init view reservations button
-        ImageIcon viewReservationsIcon = new ImageIcon("../../../resources/button-icons/reservations-icon.png");
-        JButton viewReservationsButton = new JButton(viewReservationsIcon);
-        viewReservationsButton.setPreferredSize(buttonSize);
-        viewReservationsButton.setMaximumSize(buttonSize);
+        try {
+            Image viewReservationsImage = ImageIO.read(new File("resources/button-icons/reservations-icon.png"));
+            ImageIcon viewReservationsIcon = new ImageIcon(viewReservationsImage);
+            JButton viewReservationsButton = new JButton(viewReservationsIcon);
+            viewReservationsButton.setPreferredSize(buttonSize);
+            viewReservationsButton.setMaximumSize(buttonSize);
+            add(viewReservationsButton);
+        } catch (Exception e) {
+            System.out.println("Error: reservations-icon.png not found");
+        }
         
         //Init view reservations button
-        ImageIcon checkoutIcon = new ImageIcon("../../../resources/button-icons/checkout-icon.png");
-        JButton checkoutButton = new JButton(checkoutIcon);
-        checkoutButton.setPreferredSize(buttonSize);
-        checkoutButton.setMaximumSize(buttonSize);
-
-        add(homeButton);
-        add(viewReservationsButton);
-        add(checkoutButton);
+        try {
+            Image viewReservationsImage = ImageIO.read(new File("resources/button-icons/reservations-icon.png"));
+            ImageIcon checkoutIcon = new ImageIcon(viewReservationsImage);
+            JButton checkoutButton = new JButton(checkoutIcon);
+            checkoutButton.setPreferredSize(buttonSize);
+            checkoutButton.setMaximumSize(buttonSize);
+            add(checkoutButton);
+        } catch (Exception e) {
+            System.out.println("Error: reservations-icon.png not found");
+        }
 
     }
 }
