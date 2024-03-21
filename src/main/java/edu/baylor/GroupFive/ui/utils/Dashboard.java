17b055d8ef0e3927e9a92ebdc406155650ef746e
Brendon Newton <brendonnewton0@gmail.com>
2024-03-20 23:10:24 -0500

    Added page to HomeButton constructor

--- src/main/java/edu/baylor/GroupFive/ui/utils/Dashboard.java
+++ src/main/java/edu/baylor/GroupFive/ui/utils/Dashboard.java
@@ -1,6 +1,7 @@
 package edu.baylor.GroupFive.ui.utils;
 
 import edu.baylor.GroupFive.ui.utils.buttons.DashboardButton;
+import edu.baylor.GroupFive.ui.utils.buttons.HomeButton;
 import edu.baylor.GroupFive.ui.utils.interfaces.InputDelegate;
 
 import javax.swing.*;
@@ -11,10 +12,10 @@ public class Dashboard extends JPanel {
         setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 
         // Add buttons to the dashboard
-        add(new DashboardButton("src/main/resources/button-icons/home-icon.png"));
+        add(new HomeButton(page, "src/main/resources/button-icons/home-icon.png"));
         add(new DashboardButton("src/main/resources/button-icons/reservations-icon.png"));
         add(new DashboardButton("src/main/resources/button-icons/checkout-icon.png"));
 
-        
+
     }
 }
