b3aad3fb68344b2c9a8a7f15947cd966430316fc
Brendon Newton <brendonnewton0@gmail.com>
2024-03-20 23:12:38 -0500

    Changed Main name to test

--- src/main/java/edu/baylor/GroupFive/ui/reserveRoom/test.java
+++ src/main/java/edu/baylor/GroupFive/ui/reserveRoom/test.java
@@ -5,7 +5,7 @@ import javax.swing.SwingUtilities;
 import edu.baylor.GroupFive.ui.utils.Page;
 import edu.baylor.GroupFive.ui.utils.interfaces.InputDelegate;
 
-public class Main {
+public class test {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(new Runnable() {
             public void run() {
