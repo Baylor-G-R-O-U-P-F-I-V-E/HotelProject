edd01a0ccea00fb6bdcc83cca5cb165d74b9354d
Brendon Newton <brendonnewton0@gmail.com>
2024-03-28 12:28:24 -0500

    Adding ReservationModel and getters/setters to Reservation

--- src/main/java/edu/baylor/GroupFive/models/Reservation.java
+++ src/main/java/edu/baylor/GroupFive/models/Reservation.java
@@ -1,4 +1,4 @@
-package edu.baylor.GroupFive.database.reservationDAO;
+package edu.baylor.GroupFive.models;
 
 import java.util.Date;
 
@@ -10,8 +10,6 @@ public class Reservation{
     String reservationID;
     Double price;
 
-
-
     Reservation(Date startDate, Date endDate, String guestID, String roomID, String reservationID, Double price){
         this.reservationID = reservationID;
         this.startDate = startDate;
@@ -19,6 +17,58 @@ public class Reservation{
         this.guestID = guestID;
         this.roomID = roomID;
         this.price = price;
+    }
+
+    public Date getStartDate(){
+        return startDate;
+    }
+
+    public Date getEndDate(){
+        return endDate;
+    }
+
+    public String getGuestID(){
+        return guestID;
+    }
+
+    public String getRoomID(){
+        return roomID;
+    }
+
+    public String getReservationID(){
+        return reservationID;
+    }
+
+    public Double getPrice(){
+        return price;
+    }
+
+    public void setStartDate(Date startDate){
+        this.startDate = startDate;
+    }
+
+    public void setEndDate(Date endDate){
+        this.endDate = endDate;
+    }
+
+    public void setGuestID(String guestID){
+        this.guestID = guestID;
+    }
+
+    public void setRoomID(String roomID){
+        this.roomID = roomID;
+    }
+
+    public void setReservationID(String reservationID){
+        this.reservationID = reservationID;
+    }
+
+    public void setPrice(Double price){
+        this.price = price;
+    }
+
+    public String toString(){
+        return "Reservation ID: " + reservationID + "\nStart Date: " + startDate + "\nEnd Date: " + endDate + "\nGuest ID: " + guestID + "\nRoom ID: " + roomID + "\nPrice: " + price;
     }
 
 }
---
+++ src/main/java/edu/baylor/GroupFive/ui/reservations/ReservationModel.java
@@ -0,0 +1,53 @@
+package edu.baylor.GroupFive.ui.reservations;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import edu.baylor.GroupFive.controllers.ReservationController;
+import edu.baylor.GroupFive.models.Reservation;
+import edu.baylor.GroupFive.ui.utils.interfaces.DataModel;
+import edu.baylor.GroupFive.ui.utils.table.HotelModel;
+
+public class ReservationModel extends HotelModel implements DataModel {
+
+    public ReservationModel(String[] columnNames, Class<?>[] columnClass, String path) {
+        super(columnNames, columnClass);
+
+        try {
+            //getData();
+        } catch (RuntimeException e) {
+            System.out.println(e.getLocalizedMessage());
+        }
+
+    }
+
+     public void getData() throws RuntimeException {
+       // Fetch room data from the database
+        List<Reservation> reservations = new ArrayList<>();//= ReservationController.getAllReservations();
+        
+        // Check if data was fetched successfully
+        if (reservations == null) {
+            throw new RuntimeException("Error fetching data from the database");
+        }
+
+        // Add the data to the table
+        for (Reservation reservation : reservations) {
+            try {
+                // Add the row to the table
+                addRow(new Object[] {reservation.getReservationID(), reservation.getRoomID(), formatDate(reservation.getStartDate()), formatDate(reservation.getEndDate()), reservation.getGuestID(), String.valueOf(reservation.getPrice())});
+                // Print the row to the console
+                System.out.println("Added row to table: " + reservation.getReservationID() + ", " + reservation.getRoomID() + ", " + reservation.getStartDate() + ", " + reservation.getEndDate() + ", " + reservation.getGuestID() + ", " + reservation.getPrice());
+            } catch (Exception e) {
+                // Log any errors
+                System.out.println("Error adding row to table");
+            }
+        }
+    }
+
+    public String formatDate(Date date) {
+        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+        return formatter.format(date);
+    }
+
+}
