25f47b89a3e0b2accbbdb651f9d94b2e71d339be
Icko <ickoxii@gmail.com>
2024-04-04 03:22:57 -0500

    refactored enums

---
+++ src/main/java/edu/baylor/GroupFive/models/enums/Privilege.java
@@ -0,0 +1,14 @@
+package edu.baylor.GroupFive.models.enums;
+
+public enum Privilege {
+    ADMIN, CLERK, GUEST;
+    //Turn string into enum
+    public static Privilege fromString(String text) {
+        for (Privilege b : Privilege.values()) {
+            if (b.toString().equalsIgnoreCase(text)) {
+                return b;
+            }
+        }
+        return null;
+    }
+}
