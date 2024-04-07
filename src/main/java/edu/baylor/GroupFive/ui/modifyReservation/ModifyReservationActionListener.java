9877ea79d338df8f7bfc564a8d775ce88f97860c
Icko <ickoxii@gmail.com>
2024-04-07 06:40:37 -0500

    (fix) check res dates against today

--- src/main/java/edu/baylor/GroupFive/ui/modifyReservation/ModifyReservationActionListener.java
+++ src/main/java/edu/baylor/GroupFive/ui/modifyReservation/ModifyReservationActionListener.java
@@ -4,11 +4,15 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
 import java.util.Date;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 
 import javax.swing.JTextField;
 
 import edu.baylor.GroupFive.controllers.ReservationController;
 import edu.baylor.GroupFive.models.Reservation;
+import edu.baylor.GroupFive.database.reservationDAO.ReservationDatabaseConnection;
 import edu.baylor.GroupFive.ui.utils.BadInputDialog;
 import edu.baylor.GroupFive.ui.utils.DatePanel;
 import edu.baylor.GroupFive.ui.utils.Page;
@@ -22,6 +26,14 @@ public class ModifyReservationActionListener implements ActionListener {
     private Date originalStart;
     private static String title = "Login Error";
 
+    private void makeBadInputDialog(String msg) {
+        try {
+            new BadInputDialog(msg, title);
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        }
+    }
+
     public ModifyReservationActionListener(Page loginPage, String originalRoom, Date originalStart, JTextField roomField, JTextField priceField, DatePanel startDate, DatePanel endDate) {
         this.page = loginPage;
         this.roomField = roomField;
@@ -30,11 +42,13 @@ public class ModifyReservationActionListener implements ActionListener {
         this.endDate = endDate;
         this.originalRoom = originalRoom;
         this.originalStart = originalStart;
-
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
+        // Establish connection to database
+        ReservationDatabaseConnection rdao = new ReservationDatabaseConnection();
+
         // Create a default message
         String message = "Changes successful!";
 
@@ -48,11 +62,7 @@ public class ModifyReservationActionListener implements ActionListener {
             message = """
                         Oopsie! You forgot to enter the room number.
                     """;
-            try {
-                new BadInputDialog(message, title);
-            } catch (IOException e1) {
-                e1.printStackTrace();
-            }
+            makeBadInputDialog(message);
             return;
         }
 
@@ -66,11 +76,7 @@ public class ModifyReservationActionListener implements ActionListener {
             message = """
                         Oopsie! You forgot to enter the price.
                     """;
-            try {
-                new BadInputDialog(message, title);
-            } catch (IOException e1) {
-                e1.printStackTrace();
-            }
+            makeBadInputDialog(message);
             return;
         }
 
@@ -79,17 +85,26 @@ public class ModifyReservationActionListener implements ActionListener {
         Date start = startDate.getDate();
         Date end = endDate.getDate();
 
+        // Check if start date is before today
+
+        Date today = Date.from(ZonedDateTime.now(ZoneId.of("America/Chicago")).toInstant());
+        System.out.println(today.toString());
+
+        if (start.before(today)) {
+            message = """
+                        Oopsie! The start date must be after today!
+                    """;
+            makeBadInputDialog(message);
+            return;
+        }
+
         // Check if start is after end
 
         if (start.after(end)) {
             message = """
                         Oopsie! The end date must be after the start date.
                     """;
-            try {
-                new BadInputDialog(message, title);
-            } catch (IOException e1) {
-                e1.printStackTrace();
-            }
+            makeBadInputDialog(message);
             return;
         }
 
@@ -114,11 +129,16 @@ public class ModifyReservationActionListener implements ActionListener {
             message = """
                         Oopsie! The reservation could not be updated.
                     """;
-            try {
-                new BadInputDialog(message, title);
-            } catch (IOException e1) {
-                e1.printStackTrace();
-            }
+            makeBadInputDialog(message);
+            return;
+        }
+
+        // Check if reservation is available
+        if (!rdao.checkIfAvailable(room, start, end)) {
+            message = """
+                        Oopsie! The reservation is unavailable
+                    """;
+            makeBadInputDialog(message);
             return;
         }
 
