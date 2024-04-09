5bc141477c15a589bf08d9d9b9fca8ee1f3c623b
Icko <ickoxii@gmail.com>
2024-04-09 11:50:43 -0500

    sql/util date convertor

--- src/main/java/edu/baylor/GroupFive/util/CoreUtils.java
+++ src/main/java/edu/baylor/GroupFive/util/CoreUtils.java
@@ -2,6 +2,16 @@ package edu.baylor.GroupFive.util;
 
 public class CoreUtils {
     
+    private CoreUtils() {}
+
     public static final String DATE_FORMAT = "dd/MM/yyyy";
+
+    public static java.util.Date getUtilDate(java.sql.Date date) {
+        return new java.util.Date(date.getTime());
+    }
+
+    public static java.sql.Date getSqlDate(java.util.Date date) {
+        return new java.sql.Date(date.getTime());
+    }
     
 }
