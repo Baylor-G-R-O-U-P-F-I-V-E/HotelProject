7bf786902ca1951d63bc4d174360b0de32b3ef33
Brendon Newton <brendonnewton0@gmail.com>
2024-04-19 21:34:47 -0500

    RoomDAO changes to match BaseDAO abstract class

--- src/main/java/edu/baylor/GroupFive/database/daos/BaseDAO.java
+++ src/main/java/edu/baylor/GroupFive/database/daos/BaseDAO.java
@@ -9,13 +9,14 @@ import java.sql.SQLException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 
-public abstract class BaseDAO {
+public abstract class BaseDAO<T> {
 
     private static final Logger logger = LogManager.getLogger(BaseDAO.class.getName());
-    
-    protected static Connection getConnection(){
-        
+
+    protected static Connection getConnection() {
+
         try (Connection connection = DriverManager.getConnection("jdbc:derby:FinalProject;", "", "")) {
             return connection;
 
@@ -23,7 +24,7 @@ public abstract class BaseDAO {
             logger.info("Could not connect");
             return null;
         }
-        
+
     }
 
     /**
@@ -34,4 +35,23 @@ public abstract class BaseDAO {
         DateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy");
         return dateFormat.format(myDate.getTime());
     }
+
+    public abstract T get(int id) throws SQLException;
+
+    public abstract List<T> getAll() throws SQLException;
+
+    /**
+     * Either inserts or updates based on our database
+     */
+    public abstract Integer save(T t) throws SQLException;
+
+    public abstract Integer insert(T t) throws SQLException;
+
+    public abstract Integer update(T t) throws SQLException;
+
+    /**
+     * We should not actually be deleting anything from the database except for
+     * drastic reasons
+     */
+    public abstract Integer delete(T t) throws SQLException;
 }
