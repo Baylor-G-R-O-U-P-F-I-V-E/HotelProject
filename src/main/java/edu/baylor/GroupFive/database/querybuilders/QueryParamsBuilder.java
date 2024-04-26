d09c9e1d3f5414f01c029d98d39e1ff83b7c6530
Icko <ickoxii@gmail.com>
2024-04-26 17:32:37 -0500

    javadoc db/querybuilders fin

--- src/main/java/edu/baylor/GroupFive/database/querybuilders/QueryParamsBuilder.java
+++ src/main/java/edu/baylor/GroupFive/database/querybuilders/QueryParamsBuilder.java
@@ -1,7 +1,9 @@
 package edu.baylor.GroupFive.database.querybuilders;
 
 /**
+ * Planned class for building queries with an arbitrary amount of search conditions.
  *
+ * @author Icko
  */
 public abstract class QueryParamsBuilder {
     /**
@@ -13,24 +15,30 @@ public abstract class QueryParamsBuilder {
     protected String baseQuery;
 
     /**
+     * Append a key value pair to a StringBuilder object
      *
-     * @param builder
-     * @param key
-     * @param value
+     * @param builder StringBuilder used to make query
+     * @param key Query argument
+     * @param value Value to match
+     * @author Icko
      */
     protected void append (StringBuilder builder, String key, Object value) {
     }
 
     /**
+     * Constructor for our QueryParamsBuilder object.
      *
-     * @param limit_
+     * @param limit_ Max number of results to return
+     * @author Icko
      */
     public QueryParamsBuilder(Integer limit_) {
     }
 
     /**
+     * Builds our query.
      *
-     * @return
+     * @return String output of an SQL query
+     * @author Icko
      */
     public String build() {
         return null;
--- src/main/java/edu/baylor/GroupFive/database/querybuilders/UserQueryParamsBuilder.java
+++ src/main/java/edu/baylor/GroupFive/database/querybuilders/UserQueryParamsBuilder.java
@@ -2,6 +2,11 @@ package edu.baylor.GroupFive.database.querybuilders;
 
 import java.sql.PreparedStatement;
 
+/**
+ * Planned class to generate User queries
+ *
+ * @author Icko
+ * */
 public class UserQueryParamsBuilder {
     protected String query;
     protected String firstName;
@@ -11,13 +16,15 @@ public class UserQueryParamsBuilder {
     protected String privilege;
 
     /**
+     * Initialize our query
      *
-     * @param query_
-     * @param firstName_
-     * @param lastName_
-     * @param userName_
-     * @param password_
-     * @param privilege_
+     * @param query_ Base query statement
+     * @param firstName_ Users first name
+     * @param lastName_ Users last name
+     * @param userName_ Users username
+     * @param password_ Users password
+     * @param privilege_ Users privilege level
+     * @author Icko
      */
     public UserQueryParamsBuilder(String query_, String firstName_, String lastName_, String userName_, String password_, String privilege_) {
         firstName = firstName_;
@@ -28,13 +35,15 @@ public class UserQueryParamsBuilder {
     }
 
     /**
+     * Builds a SQL query and returns a PreparedStatement with that query.
      *
-     * @param firstName
-     * @param lastName
-     * @param userName
-     * @param password
-     * @param privilege
-     * @return
+     * @param firstName Users first name
+     * @param lastName Users last name
+     * @param userName Users username
+     * @param password Users password
+     * @param privilege Users privilege level.
+     * @return PreparedStatement initialized with our query.
+     * @author Icko
      */
     public PreparedStatement build(String firstName, String lastName, String userName, String password, String privilege) {
         StringBuilder builder = new StringBuilder();
