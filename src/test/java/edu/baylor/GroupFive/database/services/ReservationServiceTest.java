5e9c70bc5305149d0daf1e381e5543c8c71b6aea
Icko <ickoxii@gmail.com>
2024-05-02 02:13:41 -0500

    checkin guest tests (mvn test didnt work tho)

--- src/test/java/edu/baylor/GroupFive/database/services/ReservationServiceTest.java
+++ src/test/java/edu/baylor/GroupFive/database/services/ReservationServiceTest.java
@@ -2,9 +2,14 @@ package edu.baylor.GroupFive.database.services;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import edu.baylor.GroupFive.database.DbSetup;
 import edu.baylor.GroupFive.models.Reservation;
+import edu.baylor.GroupFive.database.controllers.ReservationController;
+import edu.baylor.GroupFive.util.exceptions.TimeLockedOperationException;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -87,6 +92,7 @@ public class ReservationServiceTest {
     public void checkAvailable2() throws SQLException, ParseException{
         DbSetup db = new DbSetup();
         SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH);
+        ReservationServices conn = new ReservationServices();
 
         String sdate = "15/12/2024";
         String edate = "20/12/2024";
@@ -204,88 +210,59 @@ public class ReservationServiceTest {
         
     }
 
-    /**
-     *  Tests isOverlap with start2 and end2 both before start1.
-     *              s1  e1
-     *  s2  e2
-     * */
+    /*
     @Test
-    public void testIsOverlapWithNoOverlap1() {
-        Date s1 = new Date("05/01/2024");
-        Date e1 = new Date("05/06/2024");
-        Date s2 = new Date("04/24/2024");
-        Date e2 = new Date("04/30/2024");
-        assertFalse(ReservationServicesExt.isOverlap(s1, e1, s2, e2));
+    public void testValidCheckInGuest() {
+        DbSetup db = new DbSetup();
+        
+        ReservationServices rs = new ReservationServices();
+        Reservation r;
+
+        try {
+            r = rs.get(102, new Date("12/17/2024"));
+
+            // show reservation exists in the db
+            assertNotNull(r);
+
+            // Test with current date > 24 hours before start date
+            boolean b = ReservationController.checkInGuest(r, new Date("12/15/2024"));
+        } catch (SQLException e) {
+            // TODO logger?
+            System.err.println("SQLException in testValidCheckInGuest");
+        } catch (TimeLockedOperationException ex) {
+            assertTrue(false);
+        }
     }
 
-    /**
-     *  Tests isOverlap with start2 and end2 both after end2.
-     *  s1   e1
-     *              s2  e2
-     * */
     @Test
-    public void testIsOverlapWithNoOverlap2() {
-        Date s1 = new Date("05/01/2024");
-        Date e1 = new Date("05/06/2024");
-        Date s2 = new Date("05/24/2024");
-        Date e2 = new Date("05/30/2024");
-        assertFalse(ReservationServicesExt.isOverlap(s1, e1, s2, e2));
-    }
+    public void testInvalidCheckInGuest() {
+        DbSetup db = new DbSetup();
 
-    /**
-     *  Tests isOverlap with start2 before start1 and end2 after start1 but before end1.
-     *          s1      e1
-     *      s2      e2
-     * */
-    @Test
-    public void testIsOverlapWithOverlap1() {
-        Date s1 = new Date("05/01/2024");
-        Date e1 = new Date("05/06/2024");
-        Date s2 = new Date("04/24/2024");
-        Date e2 = new Date("05/03/2024");
-        assertTrue(ReservationServicesExt.isOverlap(s1, e1, s2, e2));
-    }
+        ReservationServices rs = new ReservationServices();
+        Reservation r;
 
-    /**
-     *  Tests isOverlap with start2 after start1 but before end1 and end2 after end1.
-     *          s1      e1
-     *              s2      e2
-     * */
-    @Test
-    public void testIsOverlapWithOverlap2() {
-        Date s1 = new Date("05/01/2024");
-        Date e1 = new Date("05/06/2024");
-        Date s2 = new Date("05/03/2024");
-        Date e2 = new Date("05/09/2024");
-        assertTrue(ReservationServicesExt.isOverlap(s1, e1, s2, e2));
-    }
+        try {
+            r = rs.get(103, new Date("07/12/2024"));
 
-    /**
-     *  Tests isOverlap with start2 and end2 between start1 and end1.
-     *          s1          e1
-     *              s2  e2
-     * */
-    @Test
-    public void testIsOverlapWithOverlap3() {
-        Date s1 = new Date("05/01/2024");
-        Date e1 = new Date("05/06/2024");
-        Date s2 = new Date("05/02/2024");
-        Date e2 = new Date("05/05/2024");
-        assertTrue(ReservationServicesExt.isOverlap(s1, e1, s2, e2));
-    }
 
-    /**
-     * Tests isOverlap with start2 before start1 and end2 after end1.
-     *              s1  e1
-     *          s2          e2
-     * */
-    @Test
-    public void testIsOverlapWithOverlap4() {
-        Date s1 = new Date("05/01/2024");
-        Date e1 = new Date("05/06/2024");
-        Date s2 = new Date("04/24/2024");
-        Date e2 = new Date("05/10/2024");
-        assertTrue(ReservationServicesExt.isOverlap(s1, e1, s2, e2));
+            // show reservation exists in the db
+            assertNotNull(r);
+
+            Date currDate = new Date("07/11/2024");
+            currDate.setHours(12);
+
+            boolean b = ReservationController.checkInGuest(r, currDate);
+        } catch (SQLException e) {
+            // TODO logger?
+            System.err.println("SQLException in testValidCheckInGuest");
+        } catch (TimeLockedOperationException e) {
+            System.out.println("***** PASS testInvalidCheckInGuest");
+            assertTrue(true);
+            return;
+        }
+
+        // assertTrue(false);
     }
+    */
 
 }
