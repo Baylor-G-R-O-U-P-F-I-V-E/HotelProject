8f7cfbba00b4a06f327bac2ca75d142bcf6d2105
Icko <ickoxii@gmail.com>
2024-05-04 01:37:39 -0500

    exceptions ported from broken branch

--- src/main/java/edu/baylor/GroupFive/util/exceptions/UserNotFoundException.java
+++ src/main/java/edu/baylor/GroupFive/util/exceptions/UserNotFoundException.java
@@ -8,10 +8,61 @@ package edu.baylor.GroupFive.util.exceptions;
  * @author Icko
  */
 public class UserNotFoundException extends HotelProjectException {
+
+    private static final int UNFE_CODE = 4;
+
     /**
      * Constructs a new UserNotFoundException object.
      */
     public UserNotFoundException() {
-        super("4");
+        super("UserNotFoundException encountered", UNFE_CODE);
     }
+
+    /**
+     * Constructs a new UserNotFoundException with the specified message.
+     *
+     * @param message Error message
+     * @param code Specific exception code
+     * */
+    public UserNotFoundException(String message, int code) {
+        super(message, UNFE_CODE);
+    }
+
+    /**
+     * Construct a new UserNotFoundException with the given cause.
+     *
+     * @param cause Cause of exception
+     * @param code Specific exception code
+     * */
+    public UserNotFoundException(Throwable cause, int code) {
+        super(cause, UNFE_CODE);
+    }
+
+    /**
+     * Construct a new UserNotFoundException with a specified message
+     * and given cause.
+     *
+     * @param message Error message
+     * @param cause Cause of exception
+     * @param code Specific exception code
+     * */
+    public UserNotFoundException(String message, Throwable cause, int code) {
+        super(message, cause, UNFE_CODE);
+    }
+
+    /**
+     * Construct a new UserNotFoundException with a specified message
+     * and given cause, with extra flags for designating suppression
+     * and writable stack trace status.
+     *
+     * @param message Error message
+     * @param cause Cause of exception
+     * @param enableSuppression Flag for enable/disable suppression
+     * @param writableStackTrace Flag for writable stack trace
+     * @param code Specific exception code
+     * */
+    public UserNotFoundException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace, int code) {
+        super(message, cause, enableSuppression, writableStackTrace, UNFE_CODE);
+    }
+
 }
